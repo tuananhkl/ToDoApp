fb3fc9c216055c76bbc718a82f7d352048e9f543
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2023-09-28 10:32:07 +0700

    Use Swagger outside DEV env

--- TodoAppApi/Program.cs
+++ TodoAppApi/Program.cs
@@ -15,11 +15,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
