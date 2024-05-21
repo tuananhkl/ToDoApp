20c551e9dbb57fb59f096ba4d01926fb4e5bc01b
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2024-05-21 10:55:40 +0700

    add migrate database

--- TodoAppApi/Program.cs
+++ TodoAppApi/Program.cs
@@ -39,6 +39,13 @@ try
     app.UseAuthorization();
 
     app.MapControllers();
+    
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
 
     app.Run();
 }
